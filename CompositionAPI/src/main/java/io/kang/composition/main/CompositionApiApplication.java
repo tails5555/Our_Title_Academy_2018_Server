4b03917cd7e5aaafd315d8b9fcf605660dd76ce5
tails5555 <tails5555@naver.com>
2018-06-22 22:55:41 +0900

    Create CompositionAPI Module #1 제목학원을 위한 REST API 서버 생성

---
+++ CompositionAPI/src/main/java/io/kang/composition/main/CompositionApiApplication.java
@@ -0,0 +1,12 @@
+package io.kang.composition.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompositionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompositionApiApplication.class, args);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Small_Composition
-짧은 몇 글자만으로 사람들에게 인상을 심어줄 수 있는 Web Application
+# 우리들의 제목 학원<br/><small>Our Title Academy</small>
+짧은 몇 글자만으로 사람들에게 웃음을 유발할 수 있는 Web Application 제작.
