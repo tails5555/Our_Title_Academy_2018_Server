7141a4191778ded280fcbd5ff4f96e80da319fd4
tails5555 <tails5555@naver.com>
2018-07-03 20:05:37 +0900

    Create UserAPI Module #1 제목학원을 위한 REST API 서버 생성 - 사용자 관리 API

---
+++ UserAPI/src/main/java/io/kang/main/UserApiApplication.java
@@ -0,0 +1,11 @@
+package io.kang.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApiApplication.class, args);
+    }
+}
