e792a8a72898f43e842f0ee947b0ef2533d2fc32
tails5555 <tails5555@naver.com>
2018-07-05 01:54:49 +0900

    Create UserAPI - Detail, User Unit Testing 회원 세부 정보, 회원 인증 정보 도메인 테스팅 작업 완료.

--- UserAPI/src/test/java/io/kang/unit_test/domain_unit/DetailUnitTest.java
+++ UserAPI/src/test/java/io/kang/unit_test/domain_unit/DetailUnitTest.java
@@ -1,4 +1,121 @@
 package io.kang.unit_test.domain_unit;
 
+import io.kang.domain.Age;
+import io.kang.domain.City;
+import io.kang.domain.Detail;
+import io.kang.domain.User;
+import io.kang.unit_test.domain_unit.singleton_object.AgeSingleton;
+import io.kang.unit_test.domain_unit.singleton_object.CitySingleton;
+import io.kang.unit_test.domain_unit.singleton_object.UserSingleton;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class DetailUnitTest {
+    private static final long ID = 1L;
+    private static final User USER = UserSingleton.INSTANCE.getInstance();
+    private static final String NAME = "DETAIL_NAME01";
+    private static final String EMAIL = "DETAIL_EMAIL01";
+    private static final String HOME_NUMBER = "DETAIL_HOME_NUMBER01";
+    private static final String PHONE_NUMBER = "DETAIL_PHONE_NUMBER01";
+    private static final City CITY = CitySingleton.INSTANCE.getInstance();
+    private static final Age AGE = AgeSingleton.INSTANCE.getInstance();
+
+    @Test
+    public void idGetterAndSetterTesting() throws IOException {
+        Detail detail = new Detail();
+        detail.setId(ID);
+        long id = detail.getId();
+        Assert.assertEquals(id, ID);
+    }
+
+    @Test
+    public void userGetterAndSetterTesting() throws IOException {
+        Detail detail = new Detail();
+        detail.setUser(USER);
+        User user = detail.getUser();
+        Assert.assertEquals(user, USER);
+    }
+
+    @Test
+    public void nameGetterAndSetterTesting() throws IOException {
+        Detail detail = new Detail();
+        detail.setName(NAME);
+        String name = detail.getName();
+        Assert.assertEquals(name, NAME);
+    }
+
+    @Test
+    public void emailGetterAndSetterTesting() throws IOException {
+        Detail detail = new Detail();
+        detail.setEmail(EMAIL);
+        String email = detail.getEmail();
+        Assert.assertEquals(email, EMAIL);
+    }
+
+    @Test
+    public void homeNumberGetterAndSetterTesting() throws IOException {
+        Detail detail = new Detail();
+        detail.setHomeNumber(HOME_NUMBER);
+        String homeNumber = detail.getHomeNumber();
+        Assert.assertEquals(homeNumber, HOME_NUMBER);
+    }
+
+    @Test
+    public void phoneNumberGetterAndSetterTesting() throws IOException{
+        Detail detail = new Detail();
+        detail.setPhoneNumber(PHONE_NUMBER);
+        String phoneNumber = detail.getPhoneNumber();
+        Assert.assertEquals(phoneNumber, PHONE_NUMBER);
+    }
+
+    @Test
+    public void cityGetterAndSetterTesting() throws IOException{
+        Detail detail = new Detail();
+        detail.setCity(CITY);
+        City city = detail.getCity();
+        Assert.assertEquals(city, CITY);
+    }
+
+    @Test
+    public void ageGetterAndSetterTesting() throws IOException{
+        Detail detail = new Detail();
+        detail.setAge(AGE);
+        Age age = detail.getAge();
+        Assert.assertEquals(age, AGE);
+    }
+
+    @Test
+    public void equalsTesting() throws IOException{
+        Detail detail = new Detail();
+        detail.setId(ID);
+        detail.setUser(USER);
+        detail.setName(NAME);
+        detail.setEmail(EMAIL);
+        detail.setHomeNumber(HOME_NUMBER);
+        detail.setPhoneNumber(PHONE_NUMBER);
+        detail.setCity(CITY);
+        detail.setAge(AGE);
+
+        Detail sameDetail = new Detail(ID, USER, NAME, EMAIL, HOME_NUMBER, PHONE_NUMBER, CITY, AGE);
+        Assert.assertTrue(sameDetail.equals(detail));
+    }
+
+    @Test
+    public void toStringTesting() throws IOException{
+        Detail detail = new Detail();
+        detail.setId(ID);
+        detail.setUser(USER);
+        detail.setName(NAME);
+        detail.setEmail(EMAIL);
+        detail.setHomeNumber(HOME_NUMBER);
+        detail.setPhoneNumber(PHONE_NUMBER);
+        detail.setCity(CITY);
+        detail.setAge(AGE);
+
+        String string = detail.toString();
+        String cmpResult = String.format("Detail(id=%d, user=%s, name=%s, email=%s, homeNumber=%s, phoneNumber=%s, city=%s, age=%s)", ID, USER, NAME, EMAIL, HOME_NUMBER, PHONE_NUMBER, CITY, AGE);
+        Assert.assertEquals(string, cmpResult);
+    }
 }
--- UserAPI/src/test/java/io/kang/unit_test/domain_unit/UserUnitTest.java
+++ UserAPI/src/test/java/io/kang/unit_test/domain_unit/UserUnitTest.java
@@ -1,4 +1,84 @@
 package io.kang.unit_test.domain_unit;
 
+import io.kang.domain.User;
+import io.kang.enumeration.Type;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class UserUnitTest {
+    private static final long ID = 1L;
+    private static final String LOGIN_ID = "USER_LOGIN_ID01";
+    private static final String NICKNAME = "USER_NICKNAME01";
+    private static final String PASSWORD = "USER_PASSWORD01";
+    private static final Type USER_TYPE = Type.USER;
+
+    @Test
+    public void idGetterAndSetterTesting() throws IOException {
+        User user = new User();
+        user.setId(ID);
+        long id = user.getId();
+        Assert.assertEquals(id, ID);
+    }
+
+    @Test
+    public void loginIdGetterAndSetterTesting() throws IOException {
+        User user = new User();
+        user.setLoginId(LOGIN_ID);
+        String loginId = user.getLoginId();
+        Assert.assertEquals(loginId, LOGIN_ID);
+    }
+
+    @Test
+    public void nicknameGetterAndSetterTesting() throws IOException{
+        User user = new User();
+        user.setNickname(NICKNAME);
+        String nickname = user.getNickname();
+        Assert.assertEquals(nickname, NICKNAME);
+    }
+
+    @Test
+    public void passwordGetterAndSetterTesting() throws IOException{
+        User user = new User();
+        user.setPassword(PASSWORD);
+        String password = user.getPassword();
+        Assert.assertEquals(password, PASSWORD);
+    }
+
+    @Test
+    public void typeGetterAndSetterTesting() throws IOException{
+        User user = new User();
+        user.setUserType(Type.USER);
+        Type userType = user.getUserType();
+        Assert.assertEquals(USER_TYPE, userType);
+    }
+
+    @Test
+    public void equalsTesting() throws IOException{
+        User user = new User();
+        user.setId(ID);
+        user.setLoginId(LOGIN_ID);
+        user.setNickname(NICKNAME);
+        user.setPassword(PASSWORD);
+        user.setUserType(USER_TYPE);
+
+        User sameUser = new User(ID, LOGIN_ID, NICKNAME, PASSWORD, USER_TYPE);
+
+        Assert.assertTrue(sameUser.equals(user));
+    }
+
+    @Test
+    public void toStringTesting() throws IOException{
+        User user = new User();
+        user.setId(ID);
+        user.setLoginId(LOGIN_ID);
+        user.setNickname(NICKNAME);
+        user.setPassword(PASSWORD);
+        user.setUserType(USER_TYPE);
+
+        String string = user.toString();
+        String cmpResult = String.format("User(id=%d, loginId=%s, nickname=%s, password=%s, userType=%s)", ID, LOGIN_ID, NICKNAME, PASSWORD, USER_TYPE);
+        Assert.assertEquals(string, cmpResult);
+    }
 }
