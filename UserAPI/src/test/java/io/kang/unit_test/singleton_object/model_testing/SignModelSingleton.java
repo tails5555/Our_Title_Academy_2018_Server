8074a6553db71162c30e53739057b2d3aef68a4c
tails5555 <tails5555@naver.com>
2018-07-11 20:46:43 +0900

    Create UserAPI - SignVO & SignModel Unit Testing 가입 Value Object, Model 객체 단위 테스팅 완료.

--- UserAPI/src/test/java/io/kang/unit_test/singleton_object/model_testing/SignModelSingleton.java
+++ UserAPI/src/test/java/io/kang/unit_test/singleton_object/model_testing/SignModelSingleton.java
@@ -4,10 +4,10 @@ import io.kang.model.SignModel;
 
 public enum SignModelSingleton {
     INSTANCE;
-    private SignModel signModel = new SignModel("LOGIN_ID01", "SIGN_MAIN_PASSWORD01", "SIGN_SUB_PASSWORD01", "NAME0", "SIGN_EMAIL01", "HOME_NUMBER01", "PHONE_NUMBER01", 1L, 1L);
+    private SignModel signModel = new SignModel("LOGIN_ID01", "SIGN_MAIN_PASSWORD01", "SIGN_SUB_PASSWORD01", "NAME01", "SIGN_EMAIL01", "HOME_NUMBER01", "PHONE_NUMBER01", 1L, 1L);
     public SignModel getInstance(){
         if(this.signModel == null)
-            return new SignModel("LOGIN_ID01", "SIGN_MAIN_PASSWORD01", "SIGN_SUB_PASSWORD01", "NAME0", "SIGN_EMAIL01", "HOME_NUMBER01", "PHONE_NUMBER01", 1L, 1L);
+            return new SignModel("LOGIN_ID01", "SIGN_MAIN_PASSWORD01", "SIGN_SUB_PASSWORD01", "NAME01", "SIGN_EMAIL01", "HOME_NUMBER01", "PHONE_NUMBER01", 1L, 1L);
         else return this.signModel;
     }
 }
---
+++ UserAPI/src/test/java/io/kang/unit_test/singleton_object/value_object_testing/SignVOSingleton.java
@@ -0,0 +1,13 @@
+package io.kang.unit_test.singleton_object.value_object_testing;
+
+import io.kang.vo.SignVO;
+
+public enum SignVOSingleton {
+    INSTANCE;
+    private SignVO signVO = new SignVO("LOGIN_ID01", "SIGN_MAIN_PASSWORD01", "SIGN_SUB_PASSWORD01", "NAME01", "SIGN_EMAIL01", "HOME_NUMBER01", "PHONE_NUMBER01", 1L, 1L);
+    public SignVO getInstance(){
+        if(this.signVO == null)
+            return new SignVO("LOGIN_ID01", "SIGN_MAIN_PASSWORD01", "SIGN_SUB_PASSWORD01", "NAME01", "SIGN_EMAIL01", "HOME_NUMBER01", "PHONE_NUMBER01", 1L, 1L);
+        else return this.signVO;
+    }
+}
