55bf60d9083b0b802711029ed373417426d95c10
tails5555 <tails5555@naver.com>
2018-07-11 23:26:53 +0900

    Update UserAPI - Update Repository Interface #2 UserRepository.java 에서 로그인 아이디로 찾기, 사용자 타입 별 찾기, 닉네임 포함 찾기 기능 추가.

--- UserAPI/src/main/java/io/kang/repository/UserRepository.java
+++ UserAPI/src/main/java/io/kang/repository/UserRepository.java
@@ -1,10 +1,16 @@
 package io.kang.repository;
 
 import io.kang.domain.User;
+import io.kang.enumeration.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    public Optional<User> findByLoginId(String loginId);
+    public List<User> findByUserType(Type type);
+    public List<User> findByNicknameContains(String nickname);
 }
