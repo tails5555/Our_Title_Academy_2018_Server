deb86987bf702d9391f3a32dd0c6b3b7abdae235
tails5555 <tails5555@naver.com>
2018-07-15 23:29:39 +0900

    Delete / Create UserAPI - SignVO Unit Testing / UserType Utility SignVO 객체를 이용할 필요가 없게 되어 제거하였음. 또한 Security Module 에서 회원 권한 별 ROLE_ 문자열을 가져오기 위해 이 클래스를 생성함.

--- UserAPI/src/main/java/io/kang/util/UserType.java
+++ UserAPI/src/main/java/io/kang/util/UserType.java
@@ -1,4 +1,17 @@
 package io.kang.util;
 
+import io.kang.enumeration.Type;
+
 public class UserType {
+    public static String builtToUserType(Type type){
+        switch(type){
+            case ADMIN :
+                return "ROLE_ADMIN";
+            case MANAGER :
+                return "ROLE_MANAGER";
+            case USER :
+                return "ROLE_USER";
+        }
+        return null;
+    }
 }
