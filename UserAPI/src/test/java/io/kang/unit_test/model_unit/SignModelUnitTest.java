b9fc9f0550d82d7ddb55a23ffc93e75755b3109e
tails5555 <tails5555@naver.com>
2018-07-15 21:38:08 +0900

    Update UserAPI - Update SignModel Unit Testing SignModel 클래스에 쓰인 이 메소드가 추가 되어 이를 반영하여 재작성하였음.

--- UserAPI/src/test/java/io/kang/unit_test/model_unit/SignModelUnitTest.java
+++ UserAPI/src/test/java/io/kang/unit_test/model_unit/SignModelUnitTest.java
@@ -2,24 +2,38 @@ package io.kang.unit_test.model_unit;
 
 import io.kang.model.SignModel;
 import io.kang.unit_test.singleton_object.model_testing.SignModelSingleton;
-import io.kang.unit_test.singleton_object.value_object_testing.SignVOSingleton;
-import io.kang.vo.SignVO;
+import io.kang.unit_test.singleton_object.value_object_testing.AgeVOSingleton;
+import io.kang.unit_test.singleton_object.value_object_testing.CityVOSingleton;
+import io.kang.unit_test.singleton_object.value_object_testing.DetailVOSingleton;
+import io.kang.unit_test.singleton_object.value_object_testing.UserVOSingleton;
+import io.kang.vo.DetailVO;
+import io.kang.vo.UserVO;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class SignModelUnitTest {
     private SignModel signModel = SignModelSingleton.INSTANCE.getInstance();
-    private SignVO signVO = SignVOSingleton.INSTANCE.getInstance();
 
     @Test
-    public void built_to_model_test(){
-        SignModel signModel = SignModel.builtToModel(this.signVO);
-        Assert.assertEquals(signModel, this.signModel);
+    public void is_password_equals_test(){
+       Assert.assertTrue(signModel.isPasswordEquals());
     }
 
     @Test
-    public void built_to_vo_test(){
-        SignVO signVO = SignModel.builtToVO(this.signModel);
-        Assert.assertEquals(signVO, this.signVO);
+    public void built_to_user_vo_test(){
+        UserVO userVO = new UserVO(null, signModel.getLoginId(), signModel.getNickname(), signModel.getMainPassword(), signModel.getType());
+        Assert.assertEquals(userVO, SignModel.builtToUserVO(signModel));
+    }
+
+    @Test
+    public void built_to_detail_vo_test(){
+        DetailVO detailVO = new DetailVO(null, UserVOSingleton.INSTANCE.getInstance(), signModel.getName(), signModel.getEmail(), signModel.getHomeNumber(), signModel.getPhoneNumber(), CityVOSingleton.INSTANCE.getInstance(), AgeVOSingleton.INSTANCE.getInstance());
+        Assert.assertEquals(detailVO, SignModel.builtToDetailVO(signModel, UserVOSingleton.INSTANCE.getInstance(), AgeVOSingleton.INSTANCE.getInstance(), CityVOSingleton.INSTANCE.getInstance()));
+    }
+
+    @Test
+    public void built_to_sign_model_test(){
+        SignModel tmpSignModel = SignModel.builtToSignModel(DetailVOSingleton.INSTANCE.getInstance(), AgeVOSingleton.INSTANCE.getInstance(), CityVOSingleton.INSTANCE.getInstance());
+        Assert.assertTrue(tmpSignModel != null);
     }
 }
--- UserAPI/src/test/java/io/kang/unit_test/singleton_object/model_testing/SignModelSingleton.java
+++ UserAPI/src/test/java/io/kang/unit_test/singleton_object/model_testing/SignModelSingleton.java
@@ -1,13 +1,14 @@
 package io.kang.unit_test.singleton_object.model_testing;
 
+import io.kang.enumeration.Type;
 import io.kang.model.SignModel;
 
 public enum SignModelSingleton {
     INSTANCE;
-    private SignModel signModel = new SignModel("LOGIN_ID01", "SIGN_MAIN_PASSWORD01", "SIGN_SUB_PASSWORD01", "NAME01", "SIGN_EMAIL01", "HOME_NUMBER01", "PHONE_NUMBER01", 1L, 1L);
+    private SignModel signModel = new SignModel("USER_LOGIN_ID01", "USER_PASSWORD01", "USER_PASSWORD01", "NAME1",  "NICKNAME01", "DETAIL_EMAIL01", "HOME_NUMBER01", "PHONE_NUMBER01", 1L, 1L, Type.USER);
     public SignModel getInstance(){
         if(this.signModel == null)
-            return new SignModel("LOGIN_ID01", "SIGN_MAIN_PASSWORD01", "SIGN_SUB_PASSWORD01", "NAME01", "SIGN_EMAIL01", "HOME_NUMBER01", "PHONE_NUMBER01", 1L, 1L);
+            return new SignModel("USER_LOGIN_ID01", "USER_PASSWORD01", "USER_PASSWORD01", "NAME1",  "NICKNAME01", "DETAIL_EMAIL01", "HOME_NUMBER01", "PHONE_NUMBER01", 1L, 1L, Type.USER);
         else return this.signModel;
     }
 }
