b6448cabe0f9839dfc07c574139820a914eb4dcd
tails5555 <tails5555@naver.com>
2018-08-20 16:15:50 +0900

    Create NoticeAPI - Skeleton Module NoticeAPI(공지사항, FAQ를 위한 API. 생각보다 간단한 기능으로 구성됨.) 모듈 생성 완료. pom.xml 내용은 일부 수정될 수 있음.

---
+++ NoticeAPI/src/main/java/io/kang/main/NoticeApiApplication.java
@@ -0,0 +1,12 @@
+package io.kang.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeApiApplication.class, args);
+    }
+}
